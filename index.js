c16620953691d9e1710acd4c78625ee4ade9a605
Andres Rosales <andresrosalespinzon@gmail.com>
2021-02-13 10:57:39 +0100

    loop all the links, get the name of each book and author, gotta handdle rejection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
   await page.goto('http://www.amazon.es');
@@ -12,8 +12,33 @@ const puppeteer = require('puppeteer');
 
   await page.click('.nav-search-submit input');
   await page.waitForSelector('[data-component-type=s-search-result]');
-  await page.waitFor(3000);
+  //   await page.waitForTimeout(2000);
   await page.screenshot({ path: 'amazon3.jpg' });
 
+  const amazonLinks = await page.evaluate(() => {
+    const element = document.querySelectorAll(
+      '[data-component-type=s-search-result] h2 a'
+    );
+    const links = [];
+    for (element of elements) {
+      links.push(element.href);
+    }
+    return links;
+  });
+  const books = [];
+  for (let link of links) {
+    await page.goto(link);
+    await page.waitForSelector('#productTitle');
+
+    const book = await page.evaluate(() => {
+      const temp = {};
+      temp.title = document.querySelector('#productTitle').innerText;
+      temp.author = document.querySelector('.author a').innerText;
+      return temp;
+    });
+    books.push(book);
+  }
+
+  console.log(books);
   await browser.close();
 })();
